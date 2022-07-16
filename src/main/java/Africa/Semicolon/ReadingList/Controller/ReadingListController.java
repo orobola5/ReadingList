af44dd369c335080a35f379ac88dc7d3a4636b95
Love Olanrewaju <olanrewajulove2@gmail.com>
2022-07-16 22:28:06 +0100

    A Book app using springboot/html

--- src/main/java/Africa/Semicolon/ReadingList/Controller/ReadingListController.java
+++ src/main/java/Africa/Semicolon/ReadingList/Controller/ReadingListController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/")
 public class ReadingListController {
-    private ReadingListRepository readingListRepository;
+    private final ReadingListRepository readingListRepository;
 
     @Autowired
     public ReadingListController(ReadingListRepository readingListRepository){
